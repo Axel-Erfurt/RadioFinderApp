739302e34147d9f4141f2294c390318bb9d29f55
Axel Schneider <axel99092@t-online.de>
2021-09-16 17:42:06 +0200

    Update RadioFinderApp.py
    removed unused code

--- RadioFinderApp.py
+++ RadioFinderApp.py
@@ -112,28 +112,6 @@ class RadioBrowser:
         self.fmt = fmt
         self.builder = EndPointBuilder(fmt=self.fmt)
 
-    def countries(self, filter=""):
-        endpoint = self.builder.produce_endpoint(endpoint="countries")
-        return request(endpoint)
-
-    def codecs(self, filter=""):
-        endpoint = self.builder.produce_endpoint(endpoint="codecs")
-        return request(endpoint)
-
-    def states(self, country="", filter=""):
-        endpoint = self.builder.produce_endpoint(
-            endpoint="states", country=country, filter=filter
-        )
-        return request(endpoint)
-
-    def languages(self, filter=""):
-        endpoint = self.builder.produce_endpoint(endpoint="languages", filter=filter)
-        return request(endpoint)
-
-    def tags(self, filter=""):
-        endpoint = self.builder.produce_endpoint(endpoint="tags", filter=filter)
-        return request(endpoint)
-
     def stations(self, **params):
         endpoint = self.builder.produce_endpoint(endpoint="stations")
         kwargs = {}
@@ -141,96 +119,10 @@ class RadioBrowser:
             kwargs.update({"params": params})
         return request(endpoint, **kwargs)
 
-    def stations_byid(self, id):
-        endpoint = self.builder.produce_endpoint(
-            endpoint="stations", by="byid", search_term=id
-        )
-        return request(endpoint)
-
-    def stations_byuuid(self, uuid):
-        endpoint = self.builder.produce_endpoint(
-            endpoint="stations", by="byuuid", search_term=uuid
-        )
-        return request(endpoint)
-
     def stations_byname(self, name):
         endpoint = self.builder.produce_endpoint(
             endpoint="stations", by="byname", search_term=name
         )
-        return request(endpoint)
-
-    def stations_bynameexact(self, nameexact):
-        endpoint = self.builder.produce_endpoint(
-            endpoint="stations", by="bynameexact", search_term=nameexact
-        )
-        return request(endpoint)
-
-    def stations_bycodec(self, codec):
-        endpoint = self.builder.produce_endpoint(
-            endpoint="stations", by="bycodec", search_term=codec
-        )
-        return request(endpoint)
-
-    def stations_bycodecexact(self, codecexact):
-        endpoint = self.builder.produce_endpoint(
-            endpoint="stations", by="bycodecexact", search_term=codecexact
-        )
-        return request(endpoint)
-
-    def stations_bycountry(self, country):
-        endpoint = self.builder.produce_endpoint(
-            endpoint="stations", by="bycountry", search_term=country
-        )
-        return request(endpoint)
-
-    def stations_bycountryexact(self, countryexact):
-        endpoint = self.builder.produce_endpoint(
-            endpoint="stations", by="bycountryexact", search_term=countryexact
-        )
-        return request(endpoint)
-
-    def stations_bystate(self, state):
-        endpoint = self.builder.produce_endpoint(
-            endpoint="stations", by="bystate", search_term=state
-        )
-        return request(endpoint)
-
-    def stations_bystateexact(self, stateexact):
-        endpoint = self.builder.produce_endpoint(
-            endpoint="stations", by="bystateexact", search_term=stateexact
-        )
-        return request(endpoint)
-
-    #
-    def stations_bylanguage(self, language):
-        endpoint = self.builder.produce_endpoint(
-            endpoint="stations", by="bylanguage", search_term=language
-        )
-        return request(endpoint)
-
-    def stations_bylanguageexact(self, languageexact):
-        endpoint = self.builder.produce_endpoint(
-            endpoint="stations", by="bylanguageexact", search_term=languageexact
-        )
-        return request(endpoint)
-
-    def stations_bytag(self, tag):
-        endpoint = self.builder.produce_endpoint(
-            endpoint="stations", by="bytag", search_term=tag
-        )
-        return request(endpoint)
-
-    def stations_bytagexact(self, tagexact):
-        endpoint = self.builder.produce_endpoint(
-            endpoint="stations", by="bytagexact", search_term=tagexact
-        )
-        return request(endpoint)
-
-    def playable_station(self, station_id):
-        endpoint = self.builder.produce_endpoint(
-            endpoint="playable_station", station_id=station_id, ver="v2"
-        )
-
         return request(endpoint)
 
     def station_search(self, params, **kwargs):
@@ -245,7 +137,6 @@ class Window(Gtk.ApplicationWindow):
         super(Gtk.ApplicationWindow, self).__init__()
         
         self.set_default_size(750, 400)
-        #self.set_resizable(False)
         
         # style
         provider = Gtk.CssProvider()
@@ -405,9 +296,9 @@ class Window(Gtk.ApplicationWindow):
         self.stop_button.set_sensitive(False)
         
     def on_tag(self, bus, msg):
-        taglist = msg.parse_tag()
-        if taglist:
-            if not taglist.get_string(taglist.nth_tag_name(0)).value == None:
+        if not msg == None:
+            taglist = msg.parse_tag()
+            if not taglist == None:
                 my_tag = f'{taglist.get_string(taglist.nth_tag_name(0)).value}'
                 if my_tag:
                     if not self.old_tag == my_tag and not my_tag == "None":
@@ -520,3 +411,4 @@ if __name__ == '__main__':
     window.show_all()
     window.search_entry.grab_focus()
     Gtk.main()
+    
